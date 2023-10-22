950bea8e3d43e2e56329ce09c9baa5cf988e7c80
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2023-10-22 21:54:00 +0100

    Simplify and focus the draws plot; update all created plots

--- src/app/plots/draws.py
+++ src/app/plots/draws.py
@@ -25,23 +25,25 @@ def plot(df: pd.DataFrame, area: dict, save_to: str = None) -> None:
     # ranges_per_day.reset_index(inplace=True)
     # print(ranges_per_day)
 
-    fig, axs = plt.subplots(figsize=(7, 7), nrows=2)  # 700x700 px
+    # fig, axs = plt.subplots(figsize=(7, 7), nrows=2)  # 700x700 px
+    fig, axs = plt.subplots(figsize=(7, 3))  # 700x700 px
     sns.set_theme()
     sns.axes_style('whitegrid')
 
-    p0 = sns.barplot(data=df, x='day_of_week', y='bids', estimator=sum, ax=axs[0], color='skyblue')
-    p1 = sns.histplot(data=df, x='day_of_week', hue='bids_ranges', ax=axs[1], color='skyblue', multiple='dodge')
+    # p0 = sns.barplot(data=df, x='day_of_week', y='bids', estimator=sum, ax=axs[0], color='skyblue')
+    # p1 = sns.histplot(data=df, x='day_of_week', hue='bids_ranges', ax=axs[1], color='skyblue', multiple='dodge')
+    p1 = sns.histplot(data=df, x='day_of_week', hue='bids_ranges', color='skyblue', multiple='dodge')
 
     years = list(range(df.draw_date.min().year, df.draw_date.max().year + 1))
-    p0.set(
-        title=f'Total number of bids in {area_name}, (by draw day, {years[0]}-{years[-1]})',
-        xlabel=None, ylabel='Bids counts'
-    )
+    # p0.set(
+    #     title=f'Total number of bids in {area_name}, (by draw day, {years[0]}-{years[-1]})',
+    #     xlabel=None, ylabel='Bids counts'
+    # )
     p1.set(
         title=f'Distribution of bids in {area_name}, (by draw day, {years[0]}-{years[-1]})',
         xlabel=None, ylabel='Distribution'
     )
-    plt.gcf().text(.985, .015, _dt.utcnow().strftime('FRA, %F %T UTC'), fontsize=7, color='gray', ha='right')
+    plt.gcf().text(.854, .200, _dt.utcnow().strftime('FRA,\n%F\n%T UTC'), fontsize=6, color='gray', ha='right')
 
     plt.tight_layout()
 
