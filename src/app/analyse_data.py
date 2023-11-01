bf9afa93336a17a24e14b36262012bd49b74e10e
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2023-11-01 14:05:07 +0000

    Increase data analysis code

--- src/app/analyse_data.py
+++ src/app/analyse_data.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
-from tabulate import tabulate
 from pandas import DataFrame
+from scipy.stats import ttest_ind
+from statsmodels.tsa.stattools import adfuller
+from tabulate import tabulate
+
+
+def is_stationary(df: DataFrame, column: str) -> bool:
+    """Discover if data is stationary or has a trend"""
+    alpha = .05
+    result = adfuller(df[column])
+
+    p_value = result[1]
+    return p_value < alpha
 
 
 def describe(df: DataFrame):
@@ -36,6 +47,40 @@ def describe(df: DataFrame):
     stats.append(['Q1', '{:.1f}'.format(t.iloc[0])])
     stats.append(['Q3', '{:.1f}'.format(t.iloc[1])])
 
+    stats.append(['bids are stationary', is_stationary(df, 'bids')])
+
     print('\n' + tabulate(stats, headers='firstrow', floatfmt='.1f', colalign=('left', 'right'), tablefmt='orgtbl'))
 
-    # print(df.describe())
+
+def normalize_data(df: DataFrame, column: str) -> DataFrame:
+    """Apply z-score normalization to bids and return the data frame with an additional "normal" column
+
+    This process will parse the data to have a mean of 0 and a standard deviation of 1.
+    """
+    df['normal'] = (df.bids - df.bids.mean()) / df.bids.std()
+
+    return df
+
+
+def set_moving_avg(df: DataFrame, column: str, window_size: int) -> df:
+    df['moving_avg'] = df[column].rolling(window=window_size, min_periods=1).mean()
+    return df
+
+
+# def find_change_points(df: DataFrame) -> list:
+#     window_size = int(len(df) * .05)
+#     alpha = 0.05  # Significance level
+#
+#     segments = [df.iloc[i:i + window_size] for i in range(0, len(df), window_size)]
+#     print(f'{len(segments)=}')
+#
+#     change_points = []
+#     for i in range(1, len(segments)):
+#         segment1 = segments[i - 1]['normal']
+#         segment2 = segments[i]['normal']
+#         t_stat, p_value = ttest_ind(segment1, segment2)
+#
+#         if p_value < alpha:
+#             change_points.append(i)
+#
+#     return change_points
